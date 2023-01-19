06d086011c1e1b77b9eafe38250a31238ca01e12
Zu33air <zu33air@gmail.com>
2023-01-19 10:21:07 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,85 @@
+#! /usr/bin/env node
+class Person {
+    name;
+    age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    getName() {
+        return this.name;
+    }
+}
+class Student extends Person {
+    rollNo;
+    courses = [];
+    constructor(name, rollNo, age) {
+        super(name, age);
+        this.rollNo = rollNo;
+    }
+    registerForaCourse(newCousrse) {
+        this.courses.push(newCousrse);
+    }
+}
+class Instructor extends Person {
+    salary;
+    courses = [];
+    constructor(name, age, salary) {
+        super(name, age);
+        this.salary = salary;
+    }
+    assignedCourse(instructorCourses) {
+        this.courses.push(instructorCourses);
+    }
+}
+class Course {
+    id;
+    name;
+    students = [];
+    instructor;
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    addStudent(student) {
+        this.students.push(student);
+        student.registerForaCourse(this);
+    }
+    setInstructor(instructor) {
+        this.instructor = instructor;
+        instructor.assignedCourse(this);
+    }
+}
+class Department {
+    name;
+    courses = [];
+    constructor(name) {
+        this.name = name;
+    }
+    addCourse(course) {
+        this.courses.push(course);
+    }
+}
+const student1 = new Student("Zubair ahmad", "student1", 24);
+const student2 = new Student("Jacob Green", "student2", 25);
+const instructor1 = new Instructor("Peter", 44, 45000);
+const instructor2 = new Instructor("Zia Khan", 55, 500000);
+const course1 = new Course("IOT1", "METAVERSE");
+const course2 = new Course("IOT2", "BCC");
+const department1 = new Department("Computer Science");
+department1.addCourse(course1);
+department1.addCourse(course2);
+course1.addStudent(student1);
+course1.addStudent(student2);
+course1.setInstructor(instructor1);
+course2.addStudent(student1);
+course2.addStudent(student2);
+course2.setInstructor(instructor1);
+console.log(department1);
+export {};
+//instructor1.assignedCourse("METAVERSE");
+// console.log(instructor1.age);
+// console.log(instructor1.courses);
+// console.log(course1.students);
+// console.log(student1.courses);
+// console.log(course1.instructor);
